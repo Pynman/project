24b69b5abc7b4887cdba16987c7b5bcb51004094
Pynman <pynman4@gmail.com>
2025-02-12 01:18:21 +0530

    First commit

---
+++ index .js
@@ -0,0 +1,9 @@
+const title = "Welcome to the code show in sutnga";
+const firstName = "pynman";
+const lastName = "Nongtdu";
+
+function showName() {
+    console.log(`Hello ${firstName} + ${lastName}`);
+}
+
+showName();
